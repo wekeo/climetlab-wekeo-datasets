cdf8dbeb7392bc011526990bbe5b9075c2f6a298
Germano Guerrini <germano.guerrini@exprivia.com>
2023-10-31 11:54:53 +0100

    Fixed code QA

--- climetlab_wekeo_datasets/clms/cgls_global_ndvi_v3_1km.py
+++ climetlab_wekeo_datasets/clms/cgls_global_ndvi_v3_1km.py
@@ -6,6 +6,7 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 from __future__ import annotations
+
 from climetlab.decorators import normalize
 
 from climetlab_wekeo_datasets.clms.main import Main
